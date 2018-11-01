da3010664da89b7e32e4bac4fe30b523c345b9b4
Kelvin Hsu <whkelvin@icloud.com>
2018-11-01 16:41:00 -0700

    add in mutex to pipes, and link data pools in pumps

--- Assignment1/Assignment1/Pump.cpp
+++ Assignment1/Assignment1/Pump.cpp
@@ -4,7 +4,9 @@ Pump::Pump(CRendezvous* r, string dp_name, string pipe_name, FuelTankMonitor* ft
 {
 	Rendezvous = r;
 	PumpDataPool = new CDataPool(dp_name, 1024);
+	PumpStatusPtr = (PumpStatusStruct *) (PumpDataPool->LinkDataPool());
 	PipeFromCustomer = new CTypedPipe<CustomerInfoStruct>(pipe_name, 1024);
+	PipeMutex = new CMutex(pipe_name, 1);
 	Monitor = ftm;
 }
 
@@ -13,5 +15,13 @@ int Pump::main()
 	printf("arriving at rendevous \n");
 	Rendezvous->Wait();
 	printf("go\n");
+	PipeMutex->Wait();
+	PipeFromCustomer->Read(InfoReceived);
+	PumpStatusPtr->CI = *InfoReceived;
+	PumpStatusPtr->ReadyToFuel = false;
+
+
+
+	PipeMutex->Signal();
 	return 0;
 }
--- Assignment1/Assignment1/Pump.h
+++ Assignment1/Assignment1/Pump.h
@@ -12,9 +12,11 @@ class Pump : public ActiveClass
 {
 	CDataPool *PumpDataPool;
 	PumpStatusStruct *PumpStatusPtr;
+	CustomerInfoStruct *InfoReceived;
 	CTypedPipe<CustomerInfoStruct>* PipeFromCustomer;
 	FuelTankMonitor* Monitor;
 	CRendezvous *Rendezvous;
+	CMutex *PipeMutex;
 public:
 	Pump(CRendezvous* r, string dp_name, string pipe_name, FuelTankMonitor* ftm);
 	int main();
